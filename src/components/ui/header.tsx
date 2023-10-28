4df1aad8bcfa8e1ccf2a4e5d64f80de06aa2df3d
julio3g <juliocesar.orso@gmail.com>
2023-10-28 17:16:56 -0300

    feat: ensure user goes to catalog page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,11 +10,18 @@ import {
   ShoppingCartIcon,
 } from 'lucide-react'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from './avatar'
 import { Button } from './button'
 import { Card } from './card'
 import { Separator } from './separator'
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from './sheet'
 
 export function Header() {
   const { status, data } = useSession()
@@ -74,18 +81,32 @@ export function Header() {
                 <LogOutIcon size={16} /> Fazer Logout
               </Button>
             )}
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Inicio
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Inicio
+                </Button>
+              </Link>
+            </SheetClose>
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
             </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
